279f31f7234c8bd772840434831c5679fdbb8836
David King <dave@davbo.org>
2014-03-14 14:43:36 +0000

    Override history.route to preserve name/original route
    Some tests of the reverse function

--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
-require(['jquery','backbone', 'libs/moxiejs/app/places/router', 'fastclick', 'backbone.queryparams', 'backbone.layoutmanager'], function($, Backbone, PlacesRouter, FastClick) {
+require(['jquery', 'underscore', 'backbone', 'backbone.reverse', 'libs/moxiejs/app/places/router', 'fastclick', 'backbone.queryparams', 'backbone.layoutmanager'], function($, _, Backbone, rev, PlacesRouter, FastClick) {
     $(function() {
+        Backbone.history.route = function(route, callback, name, oldRoute) {
+            this.handlers.unshift({route: route, callback: callback, name: name, oldRoute: oldRoute});
+        };
+
         // Initialse our router
         var Router = Backbone.Router.extend(PlacesRouter);
         var router = new Router({urlPrefix: '#/', followUser: false});
@@ -9,6 +13,10 @@ require(['jquery','backbone', 'libs/moxiejs/app/places/router', 'fastclick', 'ba
 
         // This kicks off the app -- discovering the hashchanges and calling routers
         Backbone.history.start();
+
+        console.log(Backbone.history.reverse('detail', {id: 'oxpoints:53255'}));
+        console.log(Backbone.history.reverse('categories', {category_name: '/university'}));
+        console.log(Backbone.history.reverse('categories'));
 
         // Include FastClick, this removes a 300ms touch event delay
         var fc = new FastClick(document.body);
