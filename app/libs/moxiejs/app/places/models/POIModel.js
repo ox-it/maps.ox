fa109400ada7759869067a645fd062b0057e0f22
David King <dave@davbo.org>
2014-03-14 16:30:44 +0000

    Improve styling on map polygons

--- app/libs/moxiejs/app/places/models/POIModel.js
+++ app/libs/moxiejs/app/places/models/POIModel.js
@@ -26,7 +26,13 @@ define(["MoxieModel", "underscore", "moxie.conf", "places/models/RTIModels", "pl
             var feature;
             if (this.has('shape')) {
                 try {
-                    feature = L.geoJson(wellknown(this.get('shape')));
+                    feature = L.geoJson(wellknown(this.get('shape')), {
+                        // color: '#002147', // Oxford blue
+                        color: '#4891dc', // Colour of our <a>
+                        weight: 2,
+                        fill: true,
+                        fillOpacity: 0.2,
+                    });
                 } catch (e) {
                     console.log("Error parsing WKT");
                     console.log(this.get('shape'));
