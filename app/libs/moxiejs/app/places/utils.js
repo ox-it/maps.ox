12086cea27a64e7c0c2993c7016a2dc7227983b5
David King <dave@davbo.org>
2014-05-15 13:47:16 +0100

    Added comments to map layer toggling code
    Clarifies the way we use this secondary layer and when we add it to the
    map.

--- app/libs/moxiejs/app/places/utils.js
+++ app/libs/moxiejs/app/places/utils.js
@@ -19,18 +19,25 @@ define(['underscore', 'moxie.conf', 'leaflet', 'moxie.position'], function(_, Mo
             var mapOptions = MoxieConf.map.options || {};
             var map = new L.map(el, mapOptions).setView([position.coords.latitude, position.coords.longitude], MoxieConf.map.defaultZoom, true);
 
-            // Add the tile layer
+            // We use 2 tile layers, one for within Oxford [shape included in
+            // MoxieConf.map.primaryLayer.bounds] and a secondary layer for the
+            // area outside of Oxford, this is configured separately in
+            // MoxieConf.map.secondaryLayer.
             var bounds = L.latLngBounds.apply(this, MoxieConf.map.primaryLayer.bounds);
             MoxieConf.map.primaryLayer.options.bounds = bounds;
             L.tileLayer(MoxieConf.map.primaryLayer.path, MoxieConf.map.primaryLayer.options).addTo(map);
             var secondaryLayer = L.tileLayer(MoxieConf.map.secondaryLayer.path, MoxieConf.map.secondaryLayer.options);
             map.on('moveend zoomend', function(ev) {
+                // The map may end up outside the bounds of the primaryLayer by
+                // either zoom or dragging the map so we listen for these.
                 var currentBounds = map.getBounds();
                 var contained = bounds.contains(currentBounds);
                 var hasSecondaryLayer = map.hasLayer(secondaryLayer);
                 if (!contained && !hasSecondaryLayer) {
+                    // Map is out of bounds & the secondary layer isn't in place
                     map.addLayer(secondaryLayer);
                 } else if (contained && hasSecondaryLayer) {
+                    // Map in Oxford bounds so remove secondaryLayer
                     map.removeLayer(secondaryLayer);
                 }
             });
