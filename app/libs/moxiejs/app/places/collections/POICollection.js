ox-it/maps.ox

commit adb8a54e263c8023fd6757c0a41ead104cedf9bd

Set default query count to 50

app/libs/moxiejs/app/places/collections/POICollection.js
@@ -7,8 +7,10 @@ define(["backbone", "core/collections/MoxieCollection", "underscore", "places/mo
 
         initialize: function(options) {
             this.options = options || {};
-            this.query = {};
         },
+
+        query: {},
+        defaultCount: conf.defaultResultCount,
 
         followingPosition: false,
 
@@ -187,6 +189,9 @@ define(["backbone", "core/collections/MoxieCollection", "underscore", "places/mo
 
         url: function() {
             var query = _.clone(this.query);
+            if (!('count' in query)) {
+                query.count = this.defaultCount;
+            }
             if (this.options.defaultQuery && _.isEmpty(query)) {
                 query = this.options.defaultQuery;
             }

app/maps.conf.js
@@ -16,6 +16,7 @@ define([], function() {
         pathFor: function(api_method) {
             return this.paths[api_method];
         },
+        defaultResultCount: 50,
         defaultLocation: {coords: DEFAULT_LOCATION},
         mapbox: {key: 'mobileox.4xjkbj4i'},
         map: {

commit 63eb8c6cc97dc5d8f7c5c89ed8571e83de43d7ff

Only apply default count if there is no query (fixes issue introduced in adb8a54e263c8023fd6757c0a41ead104cedf9bd)

app/libs/moxiejs/app/places/collections/POICollection.js
@@ -189,11 +189,11 @@ define(["backbone", "core/collections/MoxieCollection", "underscore", "places/mo
 
         url: function() {
             var query = _.clone(this.query);
-            if (!('count' in query)) {
-                query.count = this.defaultCount;
-            }
             if (this.options.defaultQuery && _.isEmpty(query)) {
                 query = this.options.defaultQuery;
+            }
+            if (!('count' in query)) {
+                query.count = this.defaultCount;
             }
             var searchPath;
             if (this.options.format && this.options.format === conf.formats.geoJSON) {