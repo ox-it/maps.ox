4e8cd0ef754f8d7797a86e4cfc96c40c148c71b9
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-02-17 14:36:34 +0000

    Always add object as first poid, start counting them from 0 and show 0 as an entrance icon.

--- app/libs/moxiejs/app/places/models/NumberedPOIModel.js
+++ app/libs/moxiejs/app/places/models/NumberedPOIModel.js
@@ -12,12 +12,12 @@ define(['leaflet', 'places/models/POIModel'], function(L, POI) {
             if (this.has('lat') && this.has('lon')) {
                 var latlng = new L.LatLng(this.get('lat'), this.get('lon'));
                 var icon = L.divIcon({
-                    html: '<div><span>' + this.get('number') + '</span></div>', iconSize: new L.Point(30, 30),
+                    html: '<div><span>' + this.get('markerText') + '</span></div>', iconSize: new L.Point(30, 30),
                     className: 'numbered-marker',
                 });
                 return new L.marker(latlng, {icon: icon, title: this.get('name')});
             }
-        },
+        }
     });
     return NumberedPOI;
 });
